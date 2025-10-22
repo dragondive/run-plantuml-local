5668708f96d916ab7d4cf72bd8298c54cc33861d
Aravind Pai <dragondive@outlook.in>
2025-10-22 14:33:21 +0530

    feat: create dynamic workflow to test cli-args and jvm-options (#18)
    fixes: #12

--- .github/workflows/scripts/generate_dynamic_test_workflow.py
+++ .github/workflows/scripts/generate_dynamic_test_workflow.py
@@ -1,8 +1,69 @@
 #!/usr/bin/env python3
 
 import argparse
+import re
 
 import yaml
+from typing import List, Dict, Tuple
+
+ACTIONS_CHECKOUT_VERSION = "v5.0.0"
+
+
+def make_step_report_status(status: str, context: str, description: str) -> Dict[str, str]:
+    return {
+        "name": f"Report '{status}' status to the trigger SHA",
+        "if": "${{ always() }}",
+        "run": (
+            "gh api repos/${{ github.repository }}/statuses/${{ inputs.trigger_sha}} "
+            f"--field state={status} "
+            f'--field context="{context}" '
+            f'--field description="{description} status: {status}" '
+        ),
+    }
+
+
+def make_step_checkout() -> Dict[str, str]:
+    return {
+        "name": "Checkout dynamic workflow",
+        "uses": f"actions/checkout@{ACTIONS_CHECKOUT_VERSION}",
+        "with": {
+            "ref": "${{ github.event.workflow_dispatch.inputs.ref }}",
+            "token": "${{ secrets.PAT_ACTIONS }}",
+        },
+    }
+
+
+def make_dynamic_test_job(
+    job_base_key: str,
+    job_base_name: str,
+    os: str,
+    plantuml_version: str,
+    user_steps: List[Dict[str, str]],
+) -> Tuple[str, Dict[str, str]]:
+    return (
+        re.sub(r"[^a-zA-Z0-9_-]", "_", f"{os}-{plantuml_version}-{job_base_key}"),
+        {
+            "name": f"[{os}, {plantuml_version}]-{job_base_name}",
+            "runs-on": os,
+            "steps": [
+                make_step_report_status(
+                    status="pending",
+                    context=f"{os}-{plantuml_version}-{job_base_key} (dynamic-test)",
+                    description=f"{os}-{plantuml_version}-{job_base_key} (dynamic-test) status: pending"
+                ),
+                make_step_checkout(),
+                *user_steps,
+                make_step_report_status(
+                    status="${{ steps.result.outcome }}",
+                    context=f"{os}-{plantuml_version}-{job_base_key} (dynamic-test)",
+                    description=(
+                        f"{os}-{plantuml_version}-{job_base_key} (dynamic-test) status: "
+                        "${{ steps.result.outcome }}"
+                    )
+                ),
+            ],
+        },
+    )
 
 
 def main():
@@ -13,6 +74,122 @@ def main():
     args = parser.parse_args()
 
     output_path = args.output
+
+    oses = [
+        "ubuntu-24.04",
+        "ubuntu-22.04",
+        "macos-15",
+        "macos-14",
+        "windows-2025",
+        "windows-2022",
+    ]
+
+    plantuml_versions = [
+        "latest",
+        "1.2025.8",
+        "1.2025.4",
+        "1.2025.3",
+    ]
+
+    jobs = {}
+    for os_ in oses:
+        for plantuml_version in plantuml_versions:
+            (job_key, job_steps) = make_dynamic_test_job(
+                job_base_key="cli-arguments-processed",
+                job_base_name="CLI arguments processed",
+                os=os_,
+                plantuml_version=plantuml_version,
+                user_steps=[
+                    {
+                        "name": "Run plantuml",
+                        "uses": "./",
+                        "with": {
+                            "version": plantuml_version,
+                            "cache-plantuml-jar": False,
+                            "cli-arguments": (
+                                "-tsvg -noerror "
+                                "-Dinput_data_file=${{ github.workspace }}/${{ env.diagram-data-path }} "
+                                "-Doutput_filename=${{ env.output-filename }} "
+                                "-o ${{ github.workspace }} "
+                                "${{ github.workspace }}/${{ env.diagram-source-path }}"
+                            ),
+                            "jvm-options": "",
+                        },
+                    },
+                    {
+                        "name": "Check result",
+                        "id": "result",
+                        "shell": "bash",
+                        "run": """
+                            if ! [ -f '${{ github.workspace }}/${{ env.output-filename }}.svg' ];
+                            then
+                                echo "::error::Diagram file not found."
+                                exit 1
+                            fi
+
+                            if [ "$(awk 'BEGIN {count=0} \
+                                    /Hello Alice!/ && /Hello Bob!/ {++count} \
+                                    END {print count}' 2>/dev/null \
+                                    '${{ github.workspace}}/${{ env.output-filename }}.svg')" -ne 1 ];
+                            then
+                                echo "::error::Diagram not created correctly."
+                                exit 1
+                            fi
+                        """,
+                    },
+                ],
+            )
+            jobs[job_key] = job_steps
+
+    for os_ in oses:
+        for plantuml_version in plantuml_versions:
+            (job_key, job_steps) = make_dynamic_test_job(
+                job_base_key="jvm-arguments-processed",
+                job_base_name="JVM arguments processed",
+                os=os_,
+                plantuml_version=plantuml_version,
+                user_steps=[
+                    {
+                        "name": "Run plantuml",
+                        "uses": "./",
+                        "with": {
+                            "version": plantuml_version,
+                            "cache-plantuml-jar": False,
+                            "cli-arguments": (
+                                "-tpng -noerror -nometadata "
+                                "-Dinput_data_file=${{ github.workspace }}/${{ env.diagram-data-path }} "
+                                "-Doutput_filename=${{ env.output-filename }} "
+                                "-o ${{ github.workspace }} "
+                                "${{ github.workspace }}/${{ env.diagram-source-path }}"
+                            ),
+                            "jvm-options": "-DPLANTUML_LIMIT_SIZE=1",
+                        },
+                    },
+                    {
+                        "name": "Check result",
+                        "id": "result",
+                        "shell": "bash",
+                        "run": """
+                            if ! [ -f '${{ github.workspace }}/${{ env.output-filename }}.png' ];
+                            then
+                                echo "::error::Diagram file not found."
+                                exit 1
+                            fi
+
+                            IMAGE_FILE_SIZE="$(ls -s \
+                                '${{ github.workspace }}/${{ env.output-filename }}.png' \
+                                | awk '{print $1}')"
+                            THRESHOLD_SIZE=200
+                            if [ "$IMAGE_FILE_SIZE" -lt 1 ] \
+                                    || [ "$IMAGE_FILE_SIZE" -gt "$THRESHOLD_SIZE" ]; then
+                                echo "::error::JVM options not processed correctly."
+                                exit 1
+                            fi
+                        """,
+                    },
+                ],
+            )
+            jobs[job_key] = job_steps
 
     dynamic_test_workflow = {
         "name": "dynamic-test: Test the action run-plantuml-local",
@@ -26,26 +203,17 @@ def main():
                     "trigger_sha": {
                         "description": "The SHA related to the trigger for this workflow.",
                         "required": True,
-                    }
+                    },
                 }
-            }
-        },
-        "jobs": {
-            "main-branch-default-job": {
-                "runs-on": "ubuntu-latest",
-                "steps": [
-                    {
-                        "name": "Default job on main branch",
-                        "run": (
-                            'echo "::notice::"'
-                            '"The default job on the main branch. "'
-                            '"The generated dynamic workflows should override it with "'
-                            '"one or more jobs."'
-                        ),
-                    }
-                ],
             },
         },
+        "env": {
+            "GH_TOKEN": "${{ github.token }}",
+            "diagram-source-path": ".github/workflows/plantuml/diagram.puml",
+            "diagram-data-path": ".github/workflows/plantuml/diagram-data.json",
+            "output-filename": "test_plantuml",
+        },
+        "jobs": jobs,
     }
 
     with open(output_path, "w") as f:
